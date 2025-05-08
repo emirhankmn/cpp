2897c410ae2cc6ed15b909d3f50a5af4ad440cd1
eakman <arcemirhanakman@gmail.com>
2025-05-09 02:08:45 +0300

    05_finito

--- c01/ex00/Zombie.cpp
+++ c01/ex00/Zombie.cpp
@@ -16,7 +16,7 @@ void Zombie::announce(void) {
     std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 Zombie::Zombie(std::string name) : name(name) {
-    name = "SAdas";
+    // name = "SAdas";
     // this->name = name;
     }
 Zombie::~Zombie(void) {
